d41367ee4d2e44a71b48145457ee52e59626feab
abdelrhman-shibl44 <abdulrhman.mahmoud44@gmail.com>
2026-01-20 09:09:40 +0200

    refactor: standardize code formatting and improve readability in app.js

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-import dotenv from 'dotenv'; // Move dotenv import to the top
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import routes from './routes/index.js';
-import { connectToDatabase } from './db/db.js';
-import createAdminUser from './utils/createAdminUser.js';
-import cookieParser from 'cookie-parser';
-import compression from 'compression';
+import dotenv from "dotenv"; // Move dotenv import to the top
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import routes from "./routes/index.js";
+import { connectToDatabase } from "./db/db.js";
+import createAdminUser from "./utils/createAdminUser.js";
+import cookieParser from "cookie-parser";
+import compression from "compression";
 dotenv.config(); // Load environment variables from .env
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:3000'
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  }),
+);
 
 app.use(express.json());
 app.use(compression());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }))
-//middleware 
+app.use(express.urlencoded({ extended: true }));
+//middleware
 
 // Connect to MongoDB
 connectToDatabase()
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     createAdminUser()
       .then(() => console.log("Predefined user created successfully"))
       .catch((error) => {
@@ -34,12 +36,17 @@ connectToDatabase()
       });
   })
   .catch((error) => {
-    console.error('MongoDB connection error:', error);
+    console.error("MongoDB connection error:", error);
   });
 
-
 // Mount the routes
-app.use('/uploads', express.static('uploads'));
-app.use('/api', routes);
+
+// Root route for backend status
+app.get("/", (req, res) => {
+  res.send("Mulingua Backend is running!");
+});
+
+app.use("/uploads", express.static("uploads"));
+app.use("/api", routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
