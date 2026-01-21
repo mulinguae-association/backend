be2ca4a3aca4de7f8de63bfc164183e99dc52de8
abdelrhman-shibl44 <abdulrhman.mahmoud44@gmail.com>
2026-01-21 09:01:25 +0200

    refactor: format code and change logout route to POST method

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,13 @@
 import express from "express";
-import { getProfile, updateProfile, login, logout, register, forgotPassword, ResetPassword } from "../controllers/authController.js";
+import {
+  getProfile,
+  updateProfile,
+  login,
+  logout,
+  register,
+  forgotPassword,
+  ResetPassword,
+} from "../controllers/authController.js";
 import authenticateUser from "../middleware/authMiddlewar.js";
 import Multer from "multer";
 
@@ -7,16 +15,21 @@ const router = express.Router();
 
 // Multer configuration for handling image uploads
 const storage = new Multer.memoryStorage();
-const upload = Multer({ storage })
+const upload = Multer({ storage });
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", logout);
+router.post("/logout", logout);
 router.get("/profile", getProfile);
 // forgot password
-router.post("/forgot-password", forgotPassword)
-router.post("/reset-password/:id/:token", ResetPassword)
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:id/:token", ResetPassword);
 // update user info
-router.put("/user/update", upload.single('profileImage'), authenticateUser, updateProfile)
+router.put(
+  "/user/update",
+  upload.single("profileImage"),
+  authenticateUser,
+  updateProfile,
+);
 
 export default router;
