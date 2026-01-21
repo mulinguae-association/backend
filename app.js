c461cadb9adce838236c77cde6a057ba65a707e7
abdelrhman-shibl44 <abdulrhman.mahmoud44@gmail.com>
2026-01-21 06:06:16 +0200

    refactor: update CORS origin to use environment variable

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
   }),
 );
 
